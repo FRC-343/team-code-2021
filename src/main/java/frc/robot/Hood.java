0f8739601281e1dc944f6a764d0f4f787668b0aa
Metal in Motion <343MetalInMotion@gmail.com>
2020-02-22 13:21:31 -0500

    add hood limit stuff

--- src/main/java/frc/robot/Hood.java
+++ src/main/java/frc/robot/Hood.java
@@ -1,23 +1,32 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Hood {
     private final Encoder m_hoodEncoder;
+    private final DigitalInput m_hoodZero;
     private final SpeedController m_hoodMotor = new Spark(RobotConstants.getInstance().kHoodMotor);
 
     public Hood() {
         if (!RobotConstants.kPractice) {
             m_hoodEncoder = new Encoder(4, 5);
+            m_hoodZero = new DigitalInput(9);
         }
     }
 
     public void aim(double height) {
-        //m_hoodEncoder.get( /*aiming code*/ );
+        // m_hoodEncoder.get( /*aiming code*/ );
+
     }
+
     public void move(double speed) {
-        m_hoodMotor.set(speed);
+        if (m_hoodZero != null && m_hoodZero.get() && speed < 0) {
+            m_hoodEncoder.reset();
+        } else {
+            m_hoodMotor.set(speed);
+        }
     }
 }
