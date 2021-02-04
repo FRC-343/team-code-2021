82daed859e08e1cc750d2ac3d63aaf8065d9c982
Metal in Motion <343MetalInMotion@gmail.com>
2021-02-03 20:07:45 -0500

    Added todo list, made shooter faster rpm, commented out old hood aimer

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 
 public class Shooter {
-    public static final double kShootSpeed = 130.00; // rev per sec
-    public static final double kShootReadySpeed = 100.0; // rev per sec
+    public static final double kShootSpeed = 125.00; // rev per sec
+    public static final double kShootReadySpeed = 115.0; // rev per sec
     public static final double kShootGarbage = 150.0; // rev per sec, for irregular values
 
     private final SpeedController m_shooter = new Spark(5);
