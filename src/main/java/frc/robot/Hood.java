c08f4dd6368b15e24578f112e1f89093b65123eb
Metal in Motion <343MetalInMotion@gmail.com>
2021-02-10 19:12:46 -0500

    Rerere did the hood formula

--- src/main/java/frc/robot/Hood.java
+++ src/main/java/frc/robot/Hood.java
@@ -12,9 +12,8 @@ public class Hood {
     private final DigitalInput m_hoodForward;
     private final SpeedController m_hoodMotor = new Spark(RobotConstants.getInstance().kHoodMotor);
 
-    private boolean m_aiming = false;
-    private boolean m_zeroing = false;
-    
+    private boolean m_aiming = false; 
+    private boolean m_zeroing = false;   
     public Hood() {
         if (!RobotConstants.kPractice) {
             m_hoodEncoder = new Encoder(4, 5, true);
@@ -24,50 +23,49 @@ public class Hood {
     }
 
     public boolean aim(double angle) {
-        return true;
-         //boolean aimed = false;
+         boolean aimed = false;
 
-        //  double target = -0.2514*angle*angle*angle-9.9227*angle*angle-71.757*angle+3752;//60 + 2*angle - 0.208*angle*angle - 0.0287*angle*angle*angle;
-        //  SmartDashboard.putNumber("hood_target", target);
+         double target = -0.0357*angle*angle*angle - 7.3381*angle*angle - 74.092*angle + 3671.8 ; //60 + 2*angle - 0.208*angle*angle - 0.0287*angle*angle*angle;
+         SmartDashboard.putNumber("hood_target", target);
 
-        //  if (m_hoodEncoder != null && m_hoodZero != null) {
-        //      if (!m_aiming) {
-        //          m_aiming = true;
-        //          m_zeroing = true;
-        //      }
+         if (m_hoodEncoder != null && m_hoodZero != null) {
+             if (!m_aiming) {
+                 m_aiming = true;
+                 m_zeroing = true;
+             }
 
-        //      if (m_hoodEncoder.getRate() > 150 || m_hoodEncoder.getRate() < -250 || m_hoodEncoder.getDistance() > 70 || m_hoodEncoder.getDistance() < -5) {
-        //          m_zeroing = true;
-        //      }
+             if (m_hoodEncoder.getRate() > 1700 || m_hoodEncoder.getRate() < -1700 || m_hoodEncoder.getDistance() > 4400 || m_hoodEncoder.getDistance() < -222) {
+                 m_zeroing = true;
+             }
 
-        //      if (m_hoodZero.get()) {
-        //          m_zeroing = false;
-        //          m_hoodEncoder.reset();
-        //      }
+             if (m_hoodZero.get()) {
+                 m_zeroing = false;
+                 m_hoodEncoder.reset();
+             }
 
-        //      if (m_zeroing) {
-        //          m_hoodMotor.set(-0.5);
-        //      }
-        //      else {
-        //          if (m_hoodForward.get()) {
-        //              m_zeroing = true;
-        //              m_hoodMotor.set(0.0);
-        //          }
-        //          else if (m_hoodEncoder.getDistance() < target) {
-        //              m_hoodMotor.set(0.5);
-        //          }
-        //          else {
-        //              m_hoodMotor.set(0.0);
-        //              aimed = true;
-        //          }
-        //      }
-        //  } else {
-        //      m_hoodMotor.set(0.0);
-        //  }
+             if (m_zeroing) {
+                 m_hoodMotor.set(0.5);
+             }
+             else {
+                 if (m_hoodForward.get()) {
+                     m_zeroing = true;
+                     m_hoodMotor.set(0.0);
+                 }
+                 else if (m_hoodEncoder.getDistance() < target) {
+                     m_hoodMotor.set(-0.5);
+                 }
+                 else {
+                     m_hoodMotor.set(0.0);
+                     aimed = true;
+                 }
+             }
+         } else {
+             m_hoodMotor.set(0.0);
+         }
 
-        //  SmartDashboard.putBoolean("hood_aimed", aimed);
+         SmartDashboard.putBoolean("hood_aimed", aimed);
 
-        //  return aimed;
+         return aimed;
     }
 
     public void move(double speed) {
