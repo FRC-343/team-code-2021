7958943238f738085185dad684a5affb6894c333
Metal in Motion <343MetalInMotion@gmail.com>
2021-03-01 18:56:37 -0500

    Barrel auto fixes

--- src/main/java/frc/robot/autonomous/AutonomousBarrel.java
+++ src/main/java/frc/robot/autonomous/AutonomousBarrel.java
@@ -26,14 +26,34 @@ public class AutonomousBarrel extends Autonomous {
     public AutonomousBarrel(Drive robotDrive, Hood aimer, Shooter shooter, SpeedController kicker,
             SpeedController hopper, SpeedController intake, DoubleSolenoid intakeLift) {
         super(robotDrive, aimer, shooter, kicker, hopper, intake, intakeLift);
-        String trajectoryJSON = "paths/BarrelRace.wpilib.json";
-        try {
-            Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-            m_trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-        } catch (IOException ex) {
-            DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
-        }
-    }
+        TrajectoryConstraint voltageConstraint = new DifferentialDriveVoltageConstraint(
+                m_robotDrive.getRightFeedforward(), m_robotDrive.getKinematics(), 11.0);
+
+        TrajectoryConfig forwardConfig = new TrajectoryConfig(0.3 * Drive.kMaxSpeed, Drive.kMaxAcceleration)
+                .setKinematics(m_robotDrive.getKinematics()).addConstraint(voltageConstraint);
+
+        // All units in meters except the ones in radians (I think) starts facing
+        // positive x by default not positive y
+
+        // new Translation2d(x, y)
+        m_trajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(2 * Math.PI)),
+                List.of(new Translation2d(3.05, 0.0),
+                new Translation2d(3.81, -0.762),
+                new Translation2d(3.05, -1.52),
+                new Translation2d(2.29, -0.762),
+                new Translation2d(3.05, 0.0),
+                new Translation2d(5.33, 0.0),
+                new Translation2d(6.1, 0.762),
+                new Translation2d(5.33, 1.52),
+                new Translation2d(4.57, 0.762),
+                new Translation2d(5.33, 0.0),
+                new Translation2d(5.71, -0.762),
+                new Translation2d(6.86, -1.52),
+                new Translation2d(7.62, -0.762),
+                new Translation2d(6.86, 0.0),
+                new Translation2d(5.33, 0.0)),
+                new Pose2d(0, 0, new Rotation2d(Math.PI)), forwardConfig);
+    }  
 
     public void autonomousPeriodic() {
         boolean running = false;
