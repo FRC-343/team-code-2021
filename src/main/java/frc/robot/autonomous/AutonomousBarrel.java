8ef4726e7cc5c41eb5a670ce57ac8686575e57c5
Metal in Motion <343MetalInMotion@gmail.com>
2021-03-01 19:22:42 -0500

    fixed auto Barrel

--- src/main/java/frc/robot/autonomous/AutonomousBarrel.java
+++ src/main/java/frc/robot/autonomous/AutonomousBarrel.java
@@ -29,11 +29,11 @@ public class AutonomousBarrel extends Autonomous {
         TrajectoryConstraint voltageConstraint = new DifferentialDriveVoltageConstraint(
                 m_robotDrive.getRightFeedforward(), m_robotDrive.getKinematics(), 11.0);
 
-        TrajectoryConfig forwardConfig = new TrajectoryConfig(0.3 * Drive.kMaxSpeed, Drive.kMaxAcceleration)
+        TrajectoryConfig forwardConfig = new TrajectoryConfig(0.5 * Drive.kMaxSpeed, Drive.kMaxAcceleration)
                 .setKinematics(m_robotDrive.getKinematics()).addConstraint(voltageConstraint);
 
-        // All units in meters except the ones in radians (I think) starts facing
-        // positive x by default not positive y
+        // All units in meters except the ones in radians (I think) 
+        // starts facing positive x by default not positive y
 
         // new Translation2d(x, y)
         m_trajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(2 * Math.PI)),
@@ -48,11 +48,11 @@ public class AutonomousBarrel extends Autonomous {
                 new Translation2d(4.57, 0.762),
                 new Translation2d(5.33, 0.0),
                 new Translation2d(5.71, -0.762),
-                new Translation2d(6.86, -1.52),
-                new Translation2d(7.62, -0.762),
-                new Translation2d(6.86, 0.0),
-                new Translation2d(5.33, 0.0)),
-                new Pose2d(0, 0, new Rotation2d(Math.PI)), forwardConfig);
+                new Translation2d(6.86, -1.3), //changed for drift
+                new Translation2d(7.62, -0.762), 
+                new Translation2d(6.86, 0.3), //changed for drift
+                new Translation2d(5.33, 0.3)), //changed for drift
+                new Pose2d(0, 0.3, new Rotation2d(Math.PI)), forwardConfig); //changed for drift
     }  
 
     public void autonomousPeriodic() {
