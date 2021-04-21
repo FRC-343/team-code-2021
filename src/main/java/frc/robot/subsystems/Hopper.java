3f7800b0af2cbdc54dacba5f0671dcd5c6d2bb10
speedster33 <62674521+speedster33@users.noreply.github.com>
2021-04-20 20:42:02 -0400

    added setHopper() method to Hopper subsystem
    (Me doing something for once)

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -1,21 +1,13 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2020 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotConstants;
 
 public class Hopper extends SubsystemBase {
+    private final Spark m_hopper = new Spark(RobotConstants.getInstance().kHopper);
 
-    public Hopper() {
-        
-    }
-
-    @Override
-    public void periodic() {
+    public void setHopper(double speed) {
+        m_hopper.set(speed);
     }
 }
