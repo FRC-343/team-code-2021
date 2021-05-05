6fe485e2cffd88c8420b198b4bcaf8a5a533e9cc
Metal in Motion <343MetalInMotion@gmail.com>
2021-05-04 20:01:08 -0400

    nitpick automonousbase

--- src/main/java/frc/robot/autonomous/AutonomousBase.java
+++ src/main/java/frc/robot/autonomous/AutonomousBase.java
@@ -8,16 +8,16 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
  * An example command that uses an example subsystem.
  */
 public class AutonomousBase extends CommandBase {
-  // private final Drive m_drive;
+  private final Drive m_drive;
 
   /**
    * TODO: replace with real info later
    */
   public AutonomousBase(Drive drive) {
-    // m_drive = drive;
+    m_drive = drive;
 
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(drive);
+    addRequirements(m_drive);
   }
 
   // Called when the command is initially scheduled.
