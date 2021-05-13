f3188c5a82af2971329cccfeb4c045382eb229a1
Metal in Motion <343MetalInMotion@gmail.com>
2021-05-13 18:55:24 -0400

    Add sendable registry details

--- src/main/java/frc/robot/subsystems/Climbing.java
+++ src/main/java/frc/robot/subsystems/Climbing.java
@@ -1,13 +1,18 @@
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climbing extends SubsystemBase {
-    private final Spark m_winch = new Spark(10);
+  private final Spark m_winch = new Spark(10);
 
-    public void setWinch(double speed) {
-      m_winch.set(speed);
-    }
+  public Climbing() {
+    SendableRegistry.setSubsystem(m_winch, this.getClass().getName());
+    SendableRegistry.setName(m_winch, "winch");
+  }
+
+  public void setWinch(double speed) {
+    m_winch.set(speed);
+  }
 }
