8b98c54193dc769d4e52e3e47917863f6ed66249
Metal in Motion <343MetalInMotion@gmail.com>
2021-06-03 19:29:46 -0400

    extends SubsystemBase

--- src/main/java/frc/robot/subsystems/Wheel.java
+++ src/main/java/frc/robot/subsystems/Wheel.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Wheel {
+public class Wheel extends SubsystemBase{
     private final Spark m_wheel = new Spark(11);
     private final DoubleSolenoid m_wheelLift = new DoubleSolenoid(1, 6, 7);
 
