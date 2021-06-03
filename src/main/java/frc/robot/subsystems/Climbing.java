9d0016695dd4ebdc73ba10baea9a841150500af6
Metal in Motion <343MetalInMotion@gmail.com>
2021-06-03 18:56:43 -0400

    added limit switches for climbing although not wired yet

--- src/main/java/frc/robot/subsystems/Climbing.java
+++ src/main/java/frc/robot/subsystems/Climbing.java
@@ -1,15 +1,23 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climbing extends SubsystemBase {
   private final Spark m_winch = new Spark(10);
+  private final DigitalInput m_isBottom = new DigitalInput(10000000 /* please change */);
+  private final DigitalInput m_isTop = new DigitalInput(10000000 /* please change */);
+
 
   public Climbing() {
     SendableRegistry.setSubsystem(m_winch, this.getClass().getSimpleName());
     SendableRegistry.setName(m_winch, "Winch Motor");
+    SendableRegistry.setSubsystem(m_isBottom, this.getClass().getSimpleName());
+    SendableRegistry.setName(m_isBottom, "Bottom climber limit switch");
+    SendableRegistry.setSubsystem(m_isTop, this.getClass().getSimpleName());
+    SendableRegistry.setName(m_isTop, "Top climber limit switch");
   }
 
   public void setWinch(double speed) {
