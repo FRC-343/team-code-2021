3eaaae5e10b2879ebef4181f5846fc38b9b1a1a0
Metal in Motion <343MetalInMotion@gmail.com>
2021-06-03 19:02:17 -0400

    wheel subsystem

--- src/main/java/frc/robot/subsystems/Wheel.java
+++ src/main/java/frc/robot/subsystems/Wheel.java
@@ -4,5 +4,20 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Wheel {
-    
+    private final Spark m_wheel = new Spark(11);
+    private final DoubleSolenoid m_wheelLift = new DoubleSolenoid(1, 6, 7);
+
+    public void raise() {
+        m_wheelLift.set(Value.kReverse);
+    }
+
+    public void lower() {
+        m_wheelLift.set(Value.kForward);
+    }
+
+    public void setWheel(double speed) {
+        m_wheel.set(speed);
+    }
+
+
 }
