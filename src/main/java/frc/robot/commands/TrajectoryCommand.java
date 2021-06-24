4dff15bd4d3f7a469c8965523d7d3100b171ff32
Metal in Motion <343MetalInMotion@gmail.com>
2021-06-24 18:55:31 -0400

    Update TrajectoryCommand.java

--- src/main/java/frc/robot/commands/TrajectoryCommand.java
+++ src/main/java/frc/robot/commands/TrajectoryCommand.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
 import frc.robot.subsystems.Drive;
@@ -11,15 +11,9 @@ public class TrajectoryCommand extends RamseteCommand {
     public static final double kRamseteZeta = 0.7;
 
     public TrajectoryCommand(Trajectory trajectory, Drive drive) {
-        super(
-            trajectory,
-            drive::getPose,
-            new RamseteController(kRamseteB, kRamseteZeta),
-            drive.getLeftFeedforward(),
-            drive.getKinematics(),
-            drive::getWheelSpeeds,
-            drive.getLeftPIDController(),
-            drive.getRightPIDController(),
-            drive::setVoltages,
-            drive);
+        super(trajectory, drive::getPose, new RamseteController(kRamseteB, kRamseteZeta), drive.getLeftFeedforward(),
+                drive.getKinematics(), drive::getWheelSpeeds, drive.getLeftPIDController(),
+                drive.getRightPIDController(), drive::setVoltages, drive);
     }
+
+}
