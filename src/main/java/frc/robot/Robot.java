68162d68c93eac11aaeec3660e983c3215921045
Metal in Motion <343MetalInMotion@gmail.com>
2021-07-01 18:50:19 -0400

    Rachet button
    // Ocean man, take me by the hand, lead me to the land that you understand
    // Ocean man, the voyage to the corner of the globe is a real trip
    // Ocean man, the crust of a tan man imbibed by the sand
    // Soaking up the thirst of the land
    
    // Ocean man, can you see through the wonder of amazement at the oberman
    // Ocean man, the crust is elusive when it casts forth to the childlike man
    // Ocean man, the sequence of a life form braised in the sand
    // Soaking up the thirst of the land
    
    // Ocean man
    // Ocean man, take me by the hand, lead me to the land that you understand
    // Ocean man, the voyage to the corner of the globe is a real trip
    // Ocean man, the crust of a tan man imbibed by the sand
    // Soaking up the thirst of the land
    
    // Ocean man, can you see through the wonder of amazement at the oberman
    // Ocean man, the crust is elusive when it casts forth to the childlike man
    // Ocean man, the sequence of a life form braised in the sand
    // Soaking up the thirst of the land
    
    // Ocean man, ocean man

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -100,9 +100,9 @@ public class Robot extends TimedRobot {
     new JoystickButton(m_controller, XboxController.Button.kX.value)
         .whenPressed(new InstantCommand(m_wheel::raiseOrLower, m_wheel));
 
-    new JoystickButton(m_controller, XboxController.Button.kBack.value)
-        .whenPressed(new InstantCommand(m_climbing::toBeOrNotToBe, m_climbing));
-
+    new JoystickButton(m_stick, 6).whenPressed(new InstantCommand(m_climbing::engage, m_climbing));
+    new JoystickButton(m_stick, 7).whenPressed(new InstantCommand(m_climbing::disEngage, m_climbing));
+    
     new JoystickButton(m_controller, XboxController.Button.kBumperLeft.value).whenHeld(
         new ShootCommand(m_shooter, m_hopper, () -> m_controller.getTriggerAxis(XboxController.Hand.kRight) > 0.2));
 
