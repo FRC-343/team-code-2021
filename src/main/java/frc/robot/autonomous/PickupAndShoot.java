3f1dccd36bc57973c89af1eb97259d019c05188a
Metal in Motion <343MetalInMotion@gmail.com>
2021-07-01 19:42:56 -0400

    todo aaaaaaannnnnnnddddddddddddd

--- src/main/java/frc/robot/autonomous/PickupAndShoot.java
+++ src/main/java/frc/robot/autonomous/PickupAndShoot.java
@@ -29,58 +29,39 @@ public class PickupAndShoot extends SequentialCommandGroup {
   private static final double kIntakeDriveSpeed = 0.7;
 
   public PickupAndShoot(Drive drive, Intake intake, Hopper hopper, Vision vision, Hood hood, Shooter shooter) {
-    TrajectoryConstraint voltageConstraint = new DifferentialDriveVoltageConstraint(
-        drive.getRightFeedforward(), drive.getKinematics(), 11.0);
+    TrajectoryConstraint voltageConstraint = new DifferentialDriveVoltageConstraint(drive.getRightFeedforward(),
+        drive.getKinematics(), 11.0);
 
     // Create config for trajectory
     TrajectoryConfig forwardPickupConfig = new TrajectoryConfig(Drive.kMaxSpeed, Drive.kMaxAcceleration)
-      // Add kinematics to ensure max speed is actually obeyed
-      .setKinematics(drive.getKinematics()).setEndVelocity(0.7)
-      // Apply the voltage constraint
-      .addConstraint(voltageConstraint);
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(drive.getKinematics()).setEndVelocity(0.7)
+        // Apply the voltage constraint
+        .addConstraint(voltageConstraint);
 
     // Create config for trajectory
     TrajectoryConfig reverseShootConfig = new TrajectoryConfig(Drive.kMaxSpeed, Drive.kMaxAcceleration)
-      // Add kinematics to ensure max speed is actually obeyed
-      .setKinematics(drive.getKinematics())
-      // Apply the voltage constraint
-      .addConstraint(voltageConstraint).setReversed(true);
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(drive.getKinematics())
+        // Apply the voltage constraint
+        .addConstraint(voltageConstraint).setReversed(true);
 
     // commands in this autonomous
     addCommands(
         // drop intake
         new InstantCommand(intake::lower, intake),
         // pickup trajectory
-        new TrajectoryCommand(TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(2.0, 0, new Rotation2d(0)),
-            // Pass config
-            forwardPickupConfig), drive),
+        new TrajectoryCommand(TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)), List.of(),
+            new Pose2d(2.0, 0, new Rotation2d(0)), forwardPickupConfig), drive),
         // move forward to pick up yellow spheres
         new ParallelDeadlineGroup(
-          new SequentialCommandGroup(
-            new DriveDistanceCommand(kIntakeDriveDistance, kIntakeDriveSpeed, drive),
-            new DriveDistanceCommand(kIntakeDriveDistance, -kIntakeDriveSpeed, drive)
-            ),
-          new IntakeCommand(intake, hopper)
-          ),
+            new SequentialCommandGroup(new DriveDistanceCommand(kIntakeDriveDistance, kIntakeDriveSpeed, drive),
+                new DriveDistanceCommand(kIntakeDriveDistance, -kIntakeDriveSpeed, drive)),
+            new IntakeCommand(intake, hopper)),
         // shoot trajectory
-        new TrajectoryCommand(TrajectoryGenerator.generateTrajectory(
-              // Start at the origin facing the +X direction
-              new Pose2d(2.0, 0, new Rotation2d(0)),
-              // Pass through these two interior waypoints, making an 's' curve path
-              List.of(),
-              // End 3 meters straight ahead of where we started, facing forward
-              new Pose2d(0.2, 3.4999, new Rotation2d(135)),
-              // Pass config
-              reverseShootConfig), drive),
+        new TrajectoryCommand(TrajectoryGenerator.generateTrajectory(new Pose2d(2.0, 0, new Rotation2d(0)), List.of(),
+            new Pose2d(0.2, 3.4999, new Rotation2d(135)), reverseShootConfig), drive),
         // aim
-        new AimCommand(vision, hood, drive),
-        new ShootCommand(shooter, hopper)
-        );
+        new AimCommand(vision, hood, drive), new ShootCommand(shooter, hopper));
   }
 }
