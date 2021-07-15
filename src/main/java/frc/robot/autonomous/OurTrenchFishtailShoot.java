226b834a6d3fc2192999ee833d53ad175ed097b8
Metal in Motion <343MetalInMotion@gmail.com>
2021-07-15 19:00:53 -0400

    2 auto programs done 100%
    100% success rate

---
+++ src/main/java/frc/robot/autonomous/OurTrenchFishtailShoot.java
@@ -0,0 +1,67 @@
+package frc.robot.autonomous;
+
+import java.util.List;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.trajectory.constraint.TrajectoryConstraint;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.commands.AimCommand;
+import frc.robot.commands.DriveDistanceCommand;
+import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.ShootCommand;
+import frc.robot.commands.TrajectoryCommand;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Hood;
+import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Vision;
+
+public class OurTrenchFishtailShoot extends SequentialCommandGroup {
+  private static final double kIntakeDriveDistance = 1.22;
+  private static final double kIntakeDriveSpeed = 0.7;
+
+  public OurTrenchFishtailShoot(Drive drive, Intake intake, Hopper hopper, Vision vision, Hood hood, Shooter shooter) {
+    TrajectoryConstraint voltageConstraint = new DifferentialDriveVoltageConstraint(drive.getRightFeedforward(),
+        drive.getKinematics(), 11.0);
+
+    // Create config for trajectory
+    TrajectoryConfig forwardPickupConfig = new TrajectoryConfig(Drive.kMaxSpeed, Drive.kMaxAcceleration)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(drive.getKinematics()).setEndVelocity(0.7)
+        // Apply the voltage constraint
+        .addConstraint(voltageConstraint);
+
+    // Create config for trajectory
+    TrajectoryConfig reverseShootConfig = new TrajectoryConfig(Drive.kMaxSpeed, Drive.kMaxAcceleration)
+        // Add kinematics to ensure max speed is actually obeyed
+        .setKinematics(drive.getKinematics())
+        // Apply the voltage constraint
+        .addConstraint(voltageConstraint).setReversed(true);
+
+    // commands in this autonomous
+    addCommands(
+        // drop intake
+        new InstantCommand(intake::lower, intake),
+        // pickup trajectory
+        new TrajectoryCommand(TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)), List.of(),
+            new Pose2d(1.5, 0, new Rotation2d(0)), forwardPickupConfig), drive),
+        // move forward to pick up yellow spheres
+        new ParallelDeadlineGroup(
+            new SequentialCommandGroup(new DriveDistanceCommand(kIntakeDriveDistance, kIntakeDriveSpeed, drive),
+                new DriveDistanceCommand(kIntakeDriveDistance, -kIntakeDriveSpeed, drive)),
+            new IntakeCommand(intake, hopper)),
+        // shoot trajectory
+        new TrajectoryCommand(TrajectoryGenerator.generateTrajectory(new Pose2d(1.5, 0, new Rotation2d(0)), List.of(),
+            new Pose2d(0.5, -0.5, new Rotation2d(195)), reverseShootConfig), drive),
+        // aim
+        new AimCommand(vision, hood, drive), new ShootCommand(shooter, hopper));
+  }
+}
